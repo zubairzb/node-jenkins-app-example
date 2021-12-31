b9449b92285525ac5732a507a53bf888a254f0e7
zubairzb <76415096+zubairzb@users.noreply.github.com>
2021-12-31 13:43:21 +0530

    New version

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('New App!'))
+app.get('/', (req, res) => res.send('New Version!'))
 
 var server = app.listen(3000, () => {
     console.log("Listening on port " + server.address().port + "...");
